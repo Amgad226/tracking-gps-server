558fd10514305367462ccd4d942fce7955f83859
amjad alwattar <amgad.wr.1@gmail.com>
2026-05-01 18:29:53 +0300

    chore: update Prisma dependencies and fix database URL configuration
    - Downgraded @prisma/client and prisma to version 6.19.3 in package.json
    - Updated schema.prisma to include DATABASE_URL environment variable
    - Modified prisma.service.ts to log DATABASE_URL and removed unnecessary configurationo

--- src/services/prisma.service.ts
+++ src/services/prisma.service.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
 
-export const PrismaService = new PrismaClient({
-  accelerateUrl: process.env.DATABASE_URL,
-});
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
+
+
+export const PrismaService = new PrismaClient();
